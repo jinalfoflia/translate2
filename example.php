4b49f1056d3c405fd5fd7ceff7ee6020a8f89bd9
jinalfoflia <jinalfoflia@live.com>
2015-03-25 22:59:42 +0530

    translate2

---
+++ example.php
@@ -0,0 +1,29 @@
+<?php
+require("GTranslate.php");
+error_reporting(E_ALL);
+ini_set('display_error',1);
+
+/**
+* Example using RequestHTTP
+*/
+$translate_string = "Das ist wunderschön";
+ try{
+       $gt = new Gtranslate;
+	echo "[HTTP] Translating [$translate_string] German to English => ".$gt->german_to_english($translate_string)."\n";
+
+	/**
+	* Lets switch the request type to CURL
+	*/
+	$gt->setRequestType('curl');
+	echo "[CURL] Translating [$translate_string] German to English => ".$gt->german_to_english($translate_string)."\n";
+	$translate_string	=	'hello';
+	echo "[CURL] Translating [$translate_string] English to German=> ".$gt->english_to_german($translate_string)."\n";
+	echo "[CURL] Translating [$translate_string] English to Portuguese=> ".$gt->english_to_portuguese($translate_string)."\n";
+	echo "[CURL] Translating [$translate_string] English to Italian=> ".$gt->english_to_italian($translate_string)."\n";
+
+} catch (GTranslateException $ge)
+ {
+       echo $ge->getMessage();
+ }
+
+?>
